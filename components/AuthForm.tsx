e563089a60d3e959e064d03d8402c7be74844e20
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-01-31 21:24:55 +0600

    All

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,16 +1,102 @@
-import Link from 'next/link'
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import LogoLink from './LogoLink'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {Form} from "@/components/ui/form"
+import CustomInput from './CustomInput'
+import { authFormSchema } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
+import Link from 'next/link'
+
 
 
 const AuthForm = ({type}:{type:string}) => {
-  return (
-    <section className="auth-form">
-        <header className="flex flex-col gap-5 md:gap-8">
-            <LogoLink />
-        </header>
-    </section>
-  )
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof authFormSchema>>({
+        resolver: zodResolver(authFormSchema),
+        defaultValues: {
+        email: "",
+        password: "",
+        },
+    })
+    
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof authFormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        setIsLoading(true);
+        console.log(values)
+        setIsLoading(false);
+
+    }
+    const [user, setUser] = useState<boolean | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    // Ensure user state is only set after mounting
+    useEffect(() => {
+        setUser(false); // Set actual user status
+    }, []);
+    // Prevent hydration mismatch by not rendering until state is set
+    if (user === null) return null;
+    return (
+        <section className="auth-form">
+            <header className="flex flex-col gap-5 md:gap-8">
+                <LogoLink />
+                <div className="flex flex-col gap-1 md:gap-3">
+                    <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
+                    {user 
+                    ? 'Link Account'
+                    : type === 'sign-in'
+                    ? 'Sign In'
+                    : 'Sign Up'}
+                    </h1>
+                    <p className="text-16 font-normal text-gray-600">
+                        {
+                            user
+                            ?'Link your account to get started'
+                            :'Please enter your details'
+                        }
+                    </p>
+                </div>
+            </header>
+            {user ? 
+                <div className="flex flex-col gap-4">
+                    {/*PlaidLink*/}
+                </div>
+            :(
+                <>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <CustomInput control={form.control} type='text' name='email' label='Email' placeholder='Enter your Email' />
+                            <CustomInput control={form.control} type='password' name='password' label='Password' placeholder='Enter your Password' />
+                            <Button type="submit" className="form-btn" disabled={isLoading}>
+                                {
+                                    isLoading?(
+                                        <>
+                                        <Loader2 size={20} className="animate-spin"/> &nbsp; Loading...
+                                        </>
+                                    ):type === 'sign-in' ? 'Sign In' : 'Sign Up'
+                                }
+                            </Button>
+                        </form>
+                    </Form>
+                    <footer className="flex justify-center gap-1">
+                        <p>
+                            {
+                                type==='sign-in'
+                                ? "Don't have an account?"
+                                : "Already have an account?"
+                            }
+                        </p>
+                        <Link href={type === 'sign-in' ? '/sign-up':'/sign-in'} className='text-blue-500'> {type === 'sign-in' ? 'Sign Up':'Sign In'} </Link>
+                    </footer>
+                </>
+            )}
+        </section>
+    )
 }
 
 export default AuthForm
---
+++ components/CustomInput.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+    FormControl,
+    FormField,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+  import { Input } from "@/components/ui/input"
+  import { Control, FieldPath } from "react-hook-form"
+  import { z } from "zod"
+import { authFormSchema } from '@/lib/utils'
+
+  interface CustomInput{
+    control: Control<z.infer<typeof authFormSchema>>,
+    type: string,
+    name: FieldPath<z.infer<typeof authFormSchema>>,
+    label: string,
+    placeholder: string
+ }
+const CustomInput = ({control, type, name, label, placeholder}:CustomInput) => {
+  return (
+    <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+            <div className='form-item'>
+                <FormLabel className='form-label'>{label}</FormLabel>
+                <div className="flex w-full flex-col">
+                    <FormControl>
+                        <Input 
+                        type={type}
+                        placeholder={placeholder}
+                        className='input-class'
+                        {...field}
+                        />
+                    </FormControl>
+                    <FormMessage className='text-red-500' />
+                </div>
+            </div>
+        )}
+        />
+  )
+}
+
+export default CustomInput
