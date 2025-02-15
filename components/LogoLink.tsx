46c10a10432714fc92913d4d5c1b6ab1e1822731
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-02-15 14:16:05 +0600

    Logo

--- components/LogoLink.tsx
+++ components/LogoLink.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const LogoLink = () => {
   return (
     <Link href='/' className='mb-12 cursor-pointer flex items-center gap-2'>
-    <Image src='/icons/logo.svg' width={34} height={34} alt='Logo' className='size-[24px] max-xl:size-14'/>
+    <Image src='/icons/logo.png' width={50} height={50} alt='Logo' className='size-[50px] max-xl:size-14'/>
     <h1 className='sidebar-logo'>PBank</h1>
     <h1 className='text-3xl md:hidden'>PBank</h1>
 </Link>
