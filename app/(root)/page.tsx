3d03227e9f05caf6b47855e2250d3f9d436584a4
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-02-26 19:44:29 +0600

    All

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Home = async () => {
+const Home = async ({searchParams:{id,page}}:SearchParamProps) => {
+
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn?.$id });
+  if(!accounts) return; 
+  const accountsData = accounts?.data; 
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+  const account = await getAccount({ appwriteItemId });
   return (
     <section className='home'>
       <div className="home-content">
@@ -17,18 +24,17 @@ const Home = async () => {
             subtext="Access and manage your account and transactions efficiently."
           />
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.35}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
         Recent Transactions
       </div>
       <RightSidebar 
           user = {loggedIn}
-          transactions={[]}
-          // @ts-expect-error This suppresses the error but ensures you fix it later
-          banks={[ {currentBalance:1000.35},{currentBalance:250} ]}
+          transactions={accounts?.transactions}
+          banks={accountsData?.slice(0,2)}
         />
     </section>
   )
